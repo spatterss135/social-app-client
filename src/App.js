5540a4d3f59713ffe54fd0debf8b98e2a9b46cbd
spatterss135 <spatterss135@gmail.com>
2022-02-19 13:01:22 -0600

    Help

--- src/App.js
+++ src/App.js
@@ -37,11 +37,11 @@ function App() {
   
 
   async function TryIt(){
-      let response = await fetch(`http://localhost:3001/users/${user.name}`)
+      let response = await fetch(`http://localhost:5432/users/${user.name}`)
       let rData = await response.json()
       let userFriends = rData.friends.map(friend => friend.friend_id)
       if (userFriends.length > 0){
-        let responseTwo = await fetch(`http://localhost:3001/posts/${userFriends}`)
+        let responseTwo = await fetch(`http://localhost:5432/posts/${userFriends}`)
         let rDataTwo = await responseTwo.json()
         setPosts(rDataTwo)
       }
