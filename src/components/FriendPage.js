4f66ebeb83fd60d74bc51e31040d0bdc92dbf966
Joswag Sandoval <sergiojr100@outlook.com>
2022-02-22 10:16:02 -0600

    final touches on functionality for friends page

--- src/components/FriendPage.js
+++ src/components/FriendPage.js
@@ -5,22 +5,11 @@ const FriendPage = (posts, user) => {
     let name = useParams()
     const [friend, setFriend] = useState([])
 
+    // console.log(posts.posts.content)
+    // console.log('posts')
+    // console.log(posts)
+
     let [userFriends, setUserFriends] = useState([])
-
-    useEffect(async ()=> {
-        async function retrieveFriends(){
-            let response = await fetch(`http://localhost:3001/users/${name.name}`)
-            let rData = await response.json()
-            let userFriends = rData.friends.map(friend => friend.friend_id)
-            let responseTwo = await fetch(`http://localhost:3001/users/friends/${userFriends}`)
-            let rDataTwo = await responseTwo.json()
-            setUserFriends(rDataTwo)
-            console.log(rData)
-            }
-
-            await retrieveFriends()
-    
-       }, [])
 
     useEffect(() => {
         const fetchFriend = async () => {
@@ -30,28 +19,45 @@ const FriendPage = (posts, user) => {
         }
 
         fetchFriend()
-    }, [])
+    }, [userFriends])
 
-    let userFriendBox = userFriends.map(friend => {
+    useEffect(async ()=> {
+        async function retrieveFriends(){
+            let response = await fetch(`http://localhost:3001/users/${name.name}`)
+            let rData = await response.json()
+            let userFriends = rData.friends.map(friend => friend.friend_id)
+            let responseTwo = await fetch(`http://localhost:3001/users/friends/${userFriends}`)
+            let rDataTwo = await responseTwo.json()
+            setUserFriends(rDataTwo)
+            }
+
+            await retrieveFriends()
+    
+       }, [userFriends])
+
+    let userFriendBox = userFriends.map((friend, i) => {
         return (
-            <div>
+            <div key={i}>
                 <Link to={'/friend/'+friend.name}>{friend.name}</Link>
             </div>
         )
     })
 
-    // let userPosts = []; 
-    // if(posts){
-    //     userPosts = posts.filter(post => post.user_id === name.user_id)
-    // }
-    // let userPostsCards = userPosts.map(post => {
-    //     return (
-    //         <div className='post'>
-    //             <div >{user.name}</div>
-    //             <div className="post-content">{post.content}</div>
-    //         </div>
-    //     )
-    // })
+    let name_text = name.name
+    let posts_text = posts.posts
+    let userPosts = []; 
+    if(posts_text){
+        userPosts = posts_text.filter(post => post.user_id === friend.user_id)
+    }
+    let userPostsCards = userPosts.map((post, i) => {
+        return (
+            <div className='post' key={i}>
+                <div >{name_text}</div>
+                <div className="post-content">{post.content}</div>
+            </div>
+        )
+    })
+
 
     return(
         <div>
@@ -59,6 +65,7 @@ const FriendPage = (posts, user) => {
             <img src={friend.profile_pic}/>
             <p>Friends:</p>
             {userFriendBox}
+            {userPostsCards}
         </div>
     )
 }
