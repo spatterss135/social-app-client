7985329b52cbd10d01e453e66ba811854a768512
jhersbo <jhersbo@gmail.com>
2022-02-26 11:44:30 -0600

    navbar has some style

--- src/components/NaviBar.js
+++ src/components/NaviBar.js
@@ -1,26 +1,193 @@
 import { Link } from "react-router-dom"
 import AddNewUser from "./AddNewUser"
 import Cookies from "cookies-js"
+import { useState } from "react";
+
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+
+const pagesSignedIn = ['Home', 'Your Profile'];
+const pagesSignedOut = ['Home', 'Sign Up']
+const routesSignedIn = ['/', "/yourprofile", '/']
+const routesSignedOut = ['/', '/newuser']
 
 const NaviBar = ({user, setUser}) => {
-    function handleLogout(){
+
+    const [anchorElNav, setAnchorElNav] = useState(null);
+
+    const handleOpenNavMenu = (event) => {
+        setAnchorElNav(event.currentTarget);
+      };
+    
+      const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+      };
+
+      function handleLogout(){
         setUser(undefined)
         Cookies.set('user', undefined)
     }
-    return(
-        <div className="navContainer">
-            <nav className="navbar">
-                <ul className="main-nav">
-                        <li><Link to="/">Home</Link></li>
-                        {!user && <li><Link to="/newuser">Sign Up</Link></li>}
-                        {user && <li><Link to="/yourprofile">Your Profile</Link></li>}
-                </ul>
-                {user && <ul className="logout">
-                    <li onClick={()=> handleLogout()} ><Link to="/">Logout</Link></li>
-                </ul>}
-            </nav>
-        </div>
-    )
+    
+      return (
+        <AppBar position="static">
+          {user && <Container maxWidth="xl">
+            <Toolbar disableGutters>
+              <Typography
+                variant="h6"
+                noWrap
+                component="div"
+                sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}
+              >
+                THE SOCIAL APP
+              </Typography>
+    
+              <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                <IconButton
+                  size="large"
+                  aria-label="account of current user"
+                  aria-controls="menu-appbar"
+                  aria-haspopup="true"
+                  onClick={handleOpenNavMenu}
+                  color="inherit"
+                >
+                  <MenuIcon />
+                </IconButton>
+                <Menu
+                  id="menu-appbar"
+                  anchorEl={anchorElNav}
+                  anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'left',
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'left',
+                  }}
+                  open={Boolean(anchorElNav)}
+                  onClose={handleCloseNavMenu}
+                  sx={{
+                    display: { xs: 'block', md: 'none' },
+                  }}
+                >
+                  {pagesSignedIn.map((page) => (
+                    <MenuItem key={page} onClick={handleCloseNavMenu}>
+                      <Typography textAlign="center">
+                          {page}
+                      </Typography>
+                    </MenuItem>
+                  ))}
+                </Menu>
+              </Box>
+              <Typography
+                variant="h6"
+                noWrap
+                component="div"
+                sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}
+              >
+                LOGO
+              </Typography>
+              <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                {pagesSignedIn.map((page, i) => (
+                  <Button
+                    key={page}
+                    href={routesSignedIn[i]}
+                    onClick={handleCloseNavMenu}
+                    sx={{ my: 2, color: 'white', display: 'block' }}
+                  >{page}</Button>
+                ))}
+              </Box>
+    
+              <Box sx={{ flexGrow: 0 }}>
+                <Tooltip title="Logout">
+                    <IconButton onClick={handleLogout} sx={{ p: 0 }}>
+                    <Avatar alt={user.name} src={user.profile_pic} />
+                    </IconButton>
+                </Tooltip>
+              </Box>
+            </Toolbar>
+          </Container>
+          || <Container maxWidth="xl">
+          <Toolbar disableGutters>
+            <Typography
+              variant="h6"
+              noWrap
+              component="div"
+              sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}
+            >
+              THE SOCIAL APP
+            </Typography>
+  
+            <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleOpenNavMenu}
+                color="inherit"
+              >
+                <MenuIcon />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorElNav}
+                anchorOrigin={{
+                  vertical: 'bottom',
+                  horizontal: 'left',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'left',
+                }}
+                open={Boolean(anchorElNav)}
+                onClose={handleCloseNavMenu}
+                sx={{
+                  display: { xs: 'block', md: 'none' },
+                }}
+              >
+                {pagesSignedOut.map((page) => (
+                  <MenuItem key={page} onClick={handleCloseNavMenu}>
+                    <Typography textAlign="center">
+                        {page}
+                    </Typography>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
+            <Typography
+              variant="h6"
+              noWrap
+              component="div"
+              sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}
+            >
+              LOGO
+            </Typography>
+            <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+              {pagesSignedOut.map((page, i) => (
+                <Button
+                  key={page}
+                  href={routesSignedOut[i]}
+                  onClick={handleCloseNavMenu}
+                  sx={{ my: 2, color: 'white', display: 'block' }}
+                >{page}</Button>
+              ))}
+            </Box>
+          </Toolbar>
+        </Container>}
+        </AppBar>
+      );
 }
 
 export default NaviBar
