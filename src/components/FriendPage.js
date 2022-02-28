c599a36dbb0b6bb074f35cc243f640aba1850e7c
Joswag Sandoval <sergiojr100@outlook.com>
2022-02-27 19:50:15 -0600

    commit

--- src/components/FriendPage.js
+++ src/components/FriendPage.js
@@ -5,26 +5,24 @@ import Avatar from "@mui/material/Avatar"
 import { blue } from "@mui/material/colors"
 
 import * as React from 'react';
-import { styled } from '@mui/material/styles';
-import Box from '@mui/material/Box';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemAvatar from '@mui/material/ListItemAvatar';
-import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import Typography from '@mui/material/Typography';
-import FolderIcon from '@mui/icons-material/Folder';
-import DeleteIcon from '@mui/icons-material/Delete';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import IconButton from '@mui/material/IconButton';
+import FavoriteIcon from '@mui/icons-material/Favorite';
 
 const FriendPage = (posts, user) => {
     let name = useParams()
     console.log(name)
     const [friend, setFriend] = useState([])
     let [userFriends, setUserFriends] = useState([])
-
-    useEffect(() => {
-        
-    }, [])
 
     useEffect(async ()=> {
         async function retrieveFriends(){
@@ -71,10 +69,55 @@ const FriendPage = (posts, user) => {
     }
     let userPostsCards = userPosts.map((post, i) => {
         return (
-            <div className='post' key={i}>
-                <div> {friend.profile_pic ? <Avatar alt={name_text} src={friend.profile_pic}/> : <Avatar>{name_text.charAt(0)}</Avatar>} {name_text}</div>
-                <div className="post-content">{post.content}</div>
+            // <div className='post' key={i}>
+            //     <div> {friend.profile_pic ? <Avatar alt={name_text} src={friend.profile_pic}/> : <Avatar>{name_text.charAt(0)}</Avatar>} {name_text}</div>
+            //     <div className="post-content">{post.content}</div>
+            // </div>
+
+            <div className="posts">
+                <Card sx={{ maxWidth: 1000, minWidth: 800 }}>
+                    <CardHeader
+                        avatar={
+                            <div> 
+                                {friend.profile_pic ? <Avatar alt={name_text} src={friend.profile_pic}/> : <Avatar>{name_text.charAt(0)}</Avatar>} {name_text}
+                            </div>
+                        }
+                        
+                        // subheader="September 14, 2016"
+                    />
+                    <CardMedia
+                        // component="img"
+                        // height="194"
+                        // image="/static/images/cards/paella.jpg"
+                        // alt="Paella dish"
+                    />
+                    <CardContent>
+                        <Typography paragraph>
+                            {post.content}
+                        </Typography>
+                    </CardContent>
+                    <CardActions disableSpacing>
+                        <IconButton aria-label="add to favorites"> 
+                        <FavoriteIcon />
+                        </IconButton> 
+                    </CardActions>
+                </Card>
             </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         )
     })
 
@@ -89,6 +132,57 @@ const FriendPage = (posts, user) => {
                 {userFriendBox}
             </div>
             {userPostsCards}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         </div>
     )
 }
