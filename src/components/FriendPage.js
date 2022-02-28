6c2d5aed114175fbe64e0b7dede97165543f246c
Joswag Sandoval <sergiojr100@outlook.com>
2022-02-28 03:56:39 -0600

    styling for friend page and cleaning up

--- src/components/FriendPage.js
+++ src/components/FriendPage.js
@@ -12,15 +12,14 @@ import ListItemText from '@mui/material/ListItemText';
 import Typography from '@mui/material/Typography';
 import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
-import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
 import CardActions from '@mui/material/CardActions';
 import IconButton from '@mui/material/IconButton';
 import FavoriteIcon from '@mui/icons-material/Favorite';
+import timeStamp from "./UserFeedItem"
 
 const FriendPage = (posts, user) => {
     let name = useParams()
-    console.log(name)
     const [friend, setFriend] = useState([])
     let [userFriends, setUserFriends] = useState([])
 
@@ -67,13 +66,9 @@ const FriendPage = (posts, user) => {
     if(posts_text){
         userPosts = posts_text.filter(post => post.user_id === friend.user_id)
     }
+
     let userPostsCards = userPosts.map((post, i) => {
         return (
-            // <div className='post' key={i}>
-            //     <div> {friend.profile_pic ? <Avatar alt={name_text} src={friend.profile_pic}/> : <Avatar>{name_text.charAt(0)}</Avatar>} {name_text}</div>
-            //     <div className="post-content">{post.content}</div>
-            // </div>
-
             <div className="posts">
                 <Card sx={{ maxWidth: 1000, minWidth: 800 }}>
                     <CardHeader
@@ -82,14 +77,6 @@ const FriendPage = (posts, user) => {
                                 {friend.profile_pic ? <Avatar alt={name_text} src={friend.profile_pic}/> : <Avatar>{name_text.charAt(0)}</Avatar>} {name_text}
                             </div>
                         }
-                        
-                        // subheader="September 14, 2016"
-                    />
-                    <CardMedia
-                        // component="img"
-                        // height="194"
-                        // image="/static/images/cards/paella.jpg"
-                        // alt="Paella dish"
                     />
                     <CardContent>
                         <Typography paragraph>
@@ -103,86 +90,25 @@ const FriendPage = (posts, user) => {
                     </CardActions>
                 </Card>
             </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
         )
     })
 
     return(
-        <div>
-            {friend ? <h2>{friend.name}</h2> : <h2>nothing</h2>}
-            <img src={friend.profile_pic ? friend.profile_pic : "http://placekitten.com/250/175"}/>
-            <div className="friend-box">
-                <Typography sx={{ mt: 4, mb: 2 }} variant="h6" component="div">
-                Friends:
-                </Typography>
-                {userFriendBox}
+        <div className="conatainer">
+            <div className="friendBar">
+                {friend ? <h2>{friend.name}</h2> : <h2>nothing</h2>}
+                <img className="profilePic" src={friend.profile_pic ? friend.profile_pic : "http://placekitten.com/250/175"}/>
+                <div className="friendBox">
+                    <Typography sx={{ mt: 4, mb: 2 }} variant="h6" component="div">
+                    Friends:
+                    </Typography>
+                    {userFriendBox}
+                </div>
             </div>
-            {userPostsCards}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-            
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+            <div className="postCards">
+                {userPostsCards}
+            </div>
         </div>
     )
 }
